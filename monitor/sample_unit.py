5a4b2eaf606f17ed6feffc201c6aa9213fce080b
SimchaTeich <sim.teich@gmail.com>
2022-03-22 15:40:55 +0200

    remove PID from samples

--- monitor/sample_unit.py
+++ monitor/sample_unit.py
@@ -4,8 +4,8 @@ def win_sample():
     """
     Function return a sample of services
     for the curent time. sample is list of
-    tuple, each tuple is service with PID (just for running),
+    tuple, each tuple is service witw
     name and status.
     """
     services_list = list(psutil.win_service_iter())
-    return [(service.pid(), service.name(), psutil.win_service_get(service.name()).status()) for service in services_list] 
+    return [(service.name(), psutil.win_service_get(service.name()).status()) for service in services_list] 
