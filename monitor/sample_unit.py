4969d032bf37351aaae0b559b02016165a5d122c
amirg00 <amirg00@walla.com>
2022-03-22 21:16:52 +0200

    Add linux sample function which returns the list of tuples (status, service_name)

--- monitor/sample_unit.py
+++ monitor/sample_unit.py
@@ -1,4 +1,6 @@
 import psutil
+import subprocess
+import re
 
 def win_sample():
     """
@@ -8,4 +10,30 @@ def win_sample():
     name and status.
     """
     services_list = list(psutil.win_service_iter())
-    return [(service.name(), psutil.win_service_get(service.name()).status()) for service in services_list] 
+    return [(service.name(), psutil.win_service_get(service.name()).status()) for service in services_list]
+
+
+def linux_sample():
+    running = '+'
+    services_lst = []
+    output_str = subprocess.check_output("service --status-all", shell=True).decode("UTF-8")
+    services_list = output_str.split('\n')
+
+    for index, service in enumerate(services_list):
+        # Happens because the last \n after split
+        if service == "":
+            continue
+        service_symbols = service.split(' ')
+
+        status, service_name = service_symbols[2], service_symbols[5]
+
+        if status == running:
+            status = "Running"
+        else:
+            status = "Stopped"
+
+        services_lst.insert(index, (status, service_name))
+
+    return services_lst
+
+
