905c0b357de6350825230b243ecf1c6de70bd42f
AmirGill <amirg00@walla.com>
2022-03-24 00:47:39 +0200

    Fix bug when queue length is always zero. Fix documentation. ADD prints.

--- monitor/online_state.py
+++ monitor/online_state.py
@@ -11,19 +11,16 @@ class online_state:
 
     def write_to_logs(self, sample_q1, sample_q2):
         """
-        sends the Queue (with 2 samples!)
-        to the comparison unit.
-        after get the log - insert it to
-        'status_log.txt'
+        Function sends the Queue (with 2 samples!) to the comparison unit.
+        Afterwards gets the log - insert it to 'status_log.txt' file.
         """
         compare = comparator(sample_q1, sample_q2)
         compare.compare(sample_q1, sample_q2)
 
-
     def write_to_service_list(self, sample):
         """
-        function get sample and insert it
-        to the 'service_list.txt'
+        Function gets sample and inserts it to the 'service_list.txt' file
+        :return: None
         """
         with open("service_list.txt", "a") as f:
             for name, status in sample:
@@ -40,17 +37,21 @@ class online_state:
         while True:
             sample = get_sample_by_os()
             self.write_to_service_list(sample)
+            print(len(self.queue))
 
             # insert sample into Queue for comparison
             if len(self.queue) == 2:
                 self.queue.pop()
                 self.queue.insert(0, sample)
+                print("vlalala")
                 self.write_to_logs(self.queue[0], self.queue[1])
 
-            elif len(self.queue) == 1:
+            elif len(self.queue) == 1 or len(self.queue) == 0:
                 self.queue.insert(0, sample)
+                print("dididid")
 
             time.sleep(time_at_seconds)
+
 
     def main(self, time_at_seconds):
         # check witch type of sample to take.
