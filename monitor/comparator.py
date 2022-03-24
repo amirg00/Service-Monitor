a6b440af6c6f959abf10bb778ce1d89b03f888c4
AmirGill <amirg00@walla.com>
2022-03-24 02:48:10 +0200

    ADD condition to not include any other status except - "stopped", "running" or "killed".
    ADD dead_services method to get all new dead services from old sample.

--- monitor/comparator.py
+++ monitor/comparator.py
@@ -12,7 +12,24 @@ class comparator:
         print(diff_sample)
         with open("status_log.txt", "a") as f:
             for name, status in diff_sample:
+                print(status)
+                if status != "running" and status != "stopped":
+                    continue
                 current_time = time.ctime().replace(' ', '-')
-                # print(f"{current_time} {name} {status}")
                 f.write(f"{current_time} {name} {status}\n")
 
+            dead_serves = self.dead_services(old_sample, newest_sample)
+            for service_name in dead_serves:
+                f.write(f"{current_time} {service_name} killed")
+
+    def dead_services(self, old_sample: list, new_sample: list):
+        dead_services = []
+        for name, status in old_sample:
+            flag = False
+            for name_2, status_2 in new_sample:
+                if name == name_2:
+                    flag = True
+            if not flag:
+                dead_services.append(name)
+
+        return dead_services
