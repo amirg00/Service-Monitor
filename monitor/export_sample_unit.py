77fe7d2da0b6dd6efaec75d3cdf198ba082830b7
AmirGill <amirg00@walla.com>
2022-03-27 16:14:00 +0300

    Clean prints from code

--- monitor/export_sample_unit.py
+++ monitor/export_sample_unit.py
@@ -7,7 +7,7 @@ class export_sample_unit:
     def __init__(self, sample_1, sample_2):
         self.sample_1 = sample_1
         self.sample_2 = sample_2
-        # Epsilon of time is 0.1 seconds
+        # Epsilon of time is 5 seconds
         self.delta_time = timedelta(seconds=5)
 
     def compare_diff_export_samples(self):
@@ -25,21 +25,15 @@ class export_sample_unit:
 
                 if format_2 <= format_1 and self.compare_time_by_epsilon(format_2, format_1):
 
-                    # print(lines[::-1][index].split(' ')[0])
                     while lines[::-1][index].split(' ')[0] == line_lst[0]:
-
-                        # print(len(lines) - index)
                         line_lst = lines[::-1][index].split(' ')
-
                         service_name = ""
                         for i in range(1, len(line_lst) - 1):
                             service_name += line_lst[i]
                         sample.append((service_name, line_lst[len(line_lst) - 1]))
                         index += 1
                     break
-        # print(sample)
         f.close()
-        # print(sample)
         return sample
 
     def get_date_formatted(self, date: str):
@@ -49,9 +43,7 @@ class export_sample_unit:
         E.g.: 25-03-2022-16:01:32 ->  25-03-2022 16:01:32
         """
         formatted_date = date[:10] + ' ' + date[11:]
-        # print(formatted_date)
         return datetime.strptime(formatted_date, "%d-%m-%Y %H:%M:%S")
 
     def compare_time_by_epsilon(self, time, time_2):
-        # print(abs(time - time_2))
         return abs(time - time_2) < self.delta_time
