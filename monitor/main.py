7ef269fa5730b7611e0d7e100130ac44d5dc7c48
SimchaTeich <sim.teich@gmail.com>
2022-03-29 02:35:07 +0300

    stat change the main logic. build print_explanation function

--- monitor/main.py
+++ monitor/main.py
@@ -2,6 +2,38 @@ import argparse
 from online_state import online_state
 from offline_state import offline_state
 
+def print_explantion():
+    """
+    function prints some details about the monitor
+    """
+    explanation = """
+    Hello User,
+    You have two modes for operating the monitor: online and offline.
+    In online mode:
+        You enter a fixed number of seconds,
+        Then non-stop for the same number of seconds the system will
+        sample services on your computer, and save the samples and
+        the changes that occur between samples in log files.
+    In offline mode:
+        Enter into the system two hours in which a sample has been sampled in the past,
+        And you will be presented with an output describing a log between the two samples.
+
+    So, what mode would you like to activate?
+    Good luck!
+    """
+    print(explanation)
+    
+
+def main():
+    # print some details
+    print_explanation()
+
+    monitor_mode = get_monitor_mode()
+    
+
+
+
+
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Monitor')
     parser.add_argument('time_at_seconds', help='render time in seconds')
